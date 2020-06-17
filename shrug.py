b0ae57a0ad90d4d920c164d47d644edf599e71ae
PetrShatokhin <mr.gti28@mail.ru>
2020-06-17 11:08:17 +0500

    test version

---
+++ shrug.py
@@ -0,0 +1,23 @@
+# ¯\_(ツ)_/¯
+
+from .. import loader, utils
+
+import logging
+import asyncio
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class SHRUGMod(loader.Module)
+	"""Just shrug module"""
+	strings = {"name": "Shrug"}
+
+	def __init__(self):
+		self.name = self.strings["name"]
+
+	def config_complete(self):
+		pass
+
+	async def shrug(self, message):
+		"""This comand return: *message*, shrug"""
+		await utils.answer(message, "¯\_(ツ)_/¯")
